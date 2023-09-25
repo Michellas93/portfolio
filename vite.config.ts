0e89847a1143f5f8e9c98bf6f8fbd0feec33acb6
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-09-25 19:22:16 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), eslint()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/__tests__/setup.ts"],
+	},
+});
