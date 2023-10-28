04231580b4d91e05e8212e75ca7f372361aa2d45
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-10-28 15:40:31 +0200

    zmena cesty k logu

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
 	return (
 		<div className="w-16">
-			<img className="rounded-lg" src="/src/assets/logo.png" alt="logo" />
+			<img className="rounded-lg" src="../assets/logo.png" alt="logo" />
 		</div>
 	);
 };
