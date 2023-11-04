fb4665fa1fedba355a96b9c1c3f19ead6b7d7d6d
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-11-04 20:10:01 +0100

    changes components ButtonLink and Header

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,20 +1,22 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button, ButtonProps } from "./Button";
 
 type Props = Omit<ButtonProps, "onClick"> & {
 	link: string;
 };
 
-export const ButtonLink = ({}: Props) => {
-	// const navigate = useNavigate();
+export const ButtonLink = ({ link, ...props }: Props) => {
+	const navigate = useNavigate();
 	return (
 		<div className="flex space-x-3">
-			<Button onClick={(event) => console.log("klik", event)} type="primary">
-				Login
-			</Button>
-			<Button onClick={(event) => console.log("klik", event)} type="secondary">
-				Sign in
-			</Button>
+			<Button
+				{...props}
+				onClick={() => {
+					navigate(link);
+				}}
+			/>
 		</div>
 	);
+	{
+	}
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Button } from "./Button";
 import pohyb from "../assets/animation-3993429_640.png";
+import { ButtonLink } from "./ButtonLink";
 
 interface Props {
 	header: string;
@@ -13,13 +13,12 @@ export const Header = ({ header, headerParagraph }: Props) => {
 				<h1 className="text-6xl mt-4 ">{header}</h1>
 				<p className="leading-8 mt-4 text-slate-600 ">{headerParagraph}</p>
 				<div className="flex mt-4 space-x-3">
-					<Button onClick={(event) => console.log("klik", event)}>Login</Button>
-					<Button
-						onClick={(event) => console.log("klik", event)}
-						type="secondary"
-					>
+					<ButtonLink link="/Log in" type="primary">
+						Log in
+					</ButtonLink>
+					<ButtonLink link="/Sign in" type="secondary">
 						Sign in
-					</Button>
+					</ButtonLink>
 				</div>
 			</div>
 			<img className="w-1/3 pl-6" src={pohyb} />
